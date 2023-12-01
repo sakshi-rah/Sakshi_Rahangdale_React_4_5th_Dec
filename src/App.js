5e3e7af650d3ce550de9bacd6fa9ef61f4d444d4
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-12-01 17:22:57 +0530

    feat: social media app added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './component/Header/Header';
+import Home from './component/Homepage/Home';
+import Details from './component/Detailspage/Details';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Navigate to="/home" replace />}></Route>
+                <Route path="/home" element={<Home />} />
+                <Route path="/details/:id" element={<Details />}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+export default function Header () {
+    return(
+        <>
+        <header>    
+            <div>
+                <p>Social Media Gallary</p>
+            </div>
+        </header>
+        </>
+    )
+}
